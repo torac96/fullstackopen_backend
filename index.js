0a7c80671242718f007ea91d9168517f035f692f
LAPTOP-Q153GUHP\pietr <peter.96.torac@gmail.com>
2022-01-30 18:07:04 +0100

    prepare heroku deploiment

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.post('/api/persons', (request, response) => {
 
 
 //handle all the requests that come to PORT 3001
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
