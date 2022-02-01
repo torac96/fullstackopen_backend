86594196760fc299795423acf4edbda2c32bd28b
LAPTOP-Q153GUHP\pietr <peter.96.torac@gmail.com>
2022-02-01 08:45:55 +0100

    set project for production

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const morgan = require('morgan');
 const app = express();
 
 //i use this middleware to parse the incoming request to json
+app.use(express.static('build'))
 app.use(cors());
 app.use(express.json());
 
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
-  skip: (req, res)  => req.method !== 'POST'
-}))
-app.use(morgan('tiny', {
-  skip: (req, res)  => req.post === 'POST'
-}))
-
+morgan.token('body', (req, res) => {
+  if(Object.getOwnPropertyNames(req.body).length !== 0){
+    return JSON.stringify(req.body);
+  }
+  return null;
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
