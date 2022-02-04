0d36c5c585cf1299ff304ca630113d737eb44399
LAPTOP-Q153GUHP\pietr <peter.96.torac@gmail.com>
2022-02-04 08:29:20 +0100

    completed exercise 3.18 Phonebook database step6

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ morgan.token('body', (req, res) => {
   }
   return null;
 });
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/info', (request, response) => {
@@ -23,28 +24,30 @@ app.get('/info', (request, response) => {
     const text = `Phonebook has info for ${person.length} people <br><br>
     ${new Date().toString()}`;
     response.send(text);
-  })
+  }).catch(error => next(error))
 })
 
 //Handle the get all people request and retrive persons resource
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
-  })
+  }).catch(error => next(error))
 });
 
 app.get('/api/persons/:id', (request, response) => {
   Person.findById(request.params.id).then(person => {
     response.json(nopersonte)
-  })
+  }).catch(error => next(error))
 });
 
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndRemove(request.params.id).then(person => {
-    response.status(204).end()
-  }).catch((error) => {
-    response.status(404).end();
-  })
+    if (person) {
+      response.status(204).end()
+    } else {
+      response.status(404).end();
+    }
+  }).catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -55,26 +58,50 @@ app.post('/api/persons', (request, response) => {
       error: 'content missing'
     })
   }
-  
+
   const person = Person({
     name: body.name,
     phone: body.phone,
   })
 
-  Person.find({}).then(persons => {
-    
-    if (persons.some(person => person.name === body.name)) {
-      return response.status(400).json({
-        error: 'name must be unique'
-      })
-    }
-
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
-    })
-  })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  }).catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  if (!body.name && !body.phone) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    phone: body.phone,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 //handle all the requests that come to PORT 3001
 const PORT = process.env.PORT
